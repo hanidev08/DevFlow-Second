f5dc60e12ea86a59d80076cec2b63cf408c61938
hanidev08 <hanidev08@gmail.com>
2024-11-21 12:51:19 +0100

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Home = () => (
-  <h1 className=" text-3xl font-black text-violet-700">
-    Welcome to the world of Next.js
-  </h1>
+  <h1 className=" h1-bold">Welcome to the world of Next.js</h1>
 );
 
 export default Home;
