c95bfe08c49c55b4291b32575c191a54a1c797bd
hanidev08 <hanidev08@gmail.com>
2024-11-21 12:28:47 +0100

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => (
-  <h1 className=" text-3xl text-violet-700 font-black">
+  <h1 className=" text-3xl font-black text-violet-700">
     Welcome to the world of Next.js
   </h1>
 );
